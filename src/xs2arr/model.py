1d2c89b5e4e0e05073edfb3f832c3398d6e351f5
Ava Dean <ava.dean@york.ac.uk>
2025-06-26 19:08:59 +0100

    Ruff formatting

--- src/xs2arr/model.py
+++ src/xs2arr/model.py
@@ -4,6 +4,6 @@ from xs2arr.io import parse_lxcat_data
 class Model:
     def __init__(self, input_file: str = None):
         if input_file is None:
-            raise ValueError('No input file provided')
+            raise ValueError("No input file provided")
 
         self.data = parse_lxcat_data(input_file)
