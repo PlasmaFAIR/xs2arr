322f8176d8c3dca0e737b79ab6b09ca1b5c82dcc
Ava Dean <ava.dean@york.ac.uk>
2025-06-26 18:09:17 +0100

    Added ruff to tests dependencies and added an argument parser.

--- src/xs2arr/main.py
+++ src/xs2arr/main.py
@@ -1,9 +1,25 @@
+from argparse import ArgumentParser
 from sys import exit
 
 
 def main() -> int:
+    parser = ArgumentParser(
+        prog="Cross section to Arrhenius",
+        description="Calculates Arrhenius coefficients from a provided cross section",
+    )
+
+    parser.add_argument(
+        "input_file", type=str, default="input.txt", nargs="?", help="Input LXCat file"
+    )
+    parser.add_argument(
+        "output_file", type=str, default="output.txt", nargs="?", help="Output file"
+    )
+    parser.add_argument("-v", "--verbose", action="store_true", help="Verbose output")
+
+    args = parser.parse_args()
+
     return 0
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(main())
