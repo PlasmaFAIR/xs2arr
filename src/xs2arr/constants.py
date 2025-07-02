0b3101ebea5b992d11b2d9cc090a6bc6ec6e96c5
Ava Dean <ava.dean@york.ac.uk>
2025-07-02 16:56:42 +0100

    Added EEDF in the Model. Added constants for EEDF.

--- src/xs2arr/constants.py
+++ src/xs2arr/constants.py
@@ -1,3 +1,20 @@
+from math import gamma
+
 # CODATA 2022 values from Rev. Mod. Phys. 97, 025002.
-e = 1.602176634e-19  # Elementary charge
+q = 1.602176634e-19  # Elementary charge
 m_e = 9.1093837139e-31  # Electron mass
+
+
+# EEDF constants.
+def _beta1(x: float):
+    return gamma(2.5 / x) ** 1.5 * gamma(1.5 / x) ** -2.5
+
+
+def _beta2(x: float):
+    return gamma(2.5 / x) / gamma(1.5 / x)
+
+
+maxwellian_beta1 = _beta1(x=1.0)
+maxwellian_beta2 = _beta2(x=1.0)
+druyvesteyn_beta1 = _beta1(x=2.0)
+druyvesteyn_beta2 = _beta2(x=2.0)
--- src/xs2arr/model.py
+++ src/xs2arr/model.py
@@ -1,9 +1,55 @@
+from xs2arr.constants import (
+    druyvesteyn_beta1,
+    druyvesteyn_beta2,
+    maxwellian_beta1,
+    maxwellian_beta2,
+)
 from xs2arr.io import parse_lxcat_data
 
 
 class Model:
-    def __init__(self, lxcat_file: str | None = None):
+    def __init__(
+        self,
+        lxcat_file: str | None = None,
+        eedf_type: str = "maxwellian",
+    ):
         if lxcat_file is None:
             raise ValueError("No lxcat file provided")
+        if not isinstance(lxcat_file, str):
+            raise TypeError("lxcat_file must be of type str")
+        if not lxcat_file:
+            raise ValueError("lxcat_file cannot be an empty string")
 
         self.cross_section_set = parse_lxcat_data(lxcat_file)
+
+        self._eedf_beta1 = 0.0
+        self._eedf_beta2 = 0.0
+        self._eedf_type = None
+        self.eedf_type = eedf_type
+
+    @property
+    def eedf_type(self) -> str:
+        """Which EEDF to use: 'maxwellian' or 'druyvesteyn'."""
+        return self._eedf_type
+
+    @eedf_type.setter
+    def eedf_type(self, value: str) -> None:
+        if not isinstance(value, str):
+            raise TypeError("eedf_type must be of type str")
+        if value not in ("maxwellian", "druyvesteyn"):
+            raise ValueError("eedf_type must be 'maxwellian' or 'druyvesteyn'")
+
+        self._eedf_type = value
+
+        self._set_eedf_constants()
+
+    def _set_eedf_constants(self):
+        """Sets the constants beta1 and beta2 for the EEDF based on its type."""
+        if self._eedf_type == "maxwellian":
+            self._eedf_beta1 = maxwellian_beta1
+            self._eedf_beta2 = maxwellian_beta2
+        elif self._eedf_type == "druyvesteyn":
+            self._eedf_beta1 = druyvesteyn_beta1
+            self._eedf_beta2 = druyvesteyn_beta2
+        else:
+            raise ValueError("self._eedf_type is set incorrectly")
