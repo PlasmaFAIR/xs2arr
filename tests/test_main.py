31726c9bf701dca900c771468acb05f67e6828a5
Ava Dean <ava.dean@york.ac.uk>
2025-07-08 17:04:02 +0100

    Removed syrupy snapshot testing

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,10 +3,8 @@ from xs2arr import Model
 EXAMPLE_LXCAT_FILE = "tests/example_lxcat.txt"
 
 
-def test_model(snapshot):
+def test_model():
     """Test the robustness of the model class from an example LXCat data."""
 
     model = Model(EXAMPLE_LXCAT_FILE)
-    results = model.arrhenius()
-
-    assert results == snapshot
+    model.arrhenius()
