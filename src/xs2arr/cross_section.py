e93f6edc2afc68a0bae3f6cc38e05189877ee04c
Ava Dean <ava.dean@york.ac.uk>
2025-07-15 13:56:15 +0100

    Added error state to ignore logarithm of 0 value errors in interpolation

--- src/xs2arr/cross_section.py
+++ src/xs2arr/cross_section.py
@@ -1,13 +1,17 @@
-from numpy import concatenate, interp, log10, ndarray
+from numpy import concatenate, errstate, inf, interp, log10, ndarray, where
 
 
 def interpolate_xs(energy: ndarray, xs_energy: ndarray, xs: ndarray):
+    if any(xs < 0.0):
+        raise ValueError("All values in xs must be >= 0.0")
+
     # Add extra (0, 0) point to data.
     xs_energy = concatenate(([0.0], xs_energy))
     xs = concatenate(([0.0], xs))
 
     # We interpolate on the 10*log of the cross section.
-    xs_log = 10.0 * log10(xs)
+    with errstate(divide="ignore"):
+        xs_log = where(xs > 0.0, 10.0 * log10(xs), -inf)
 
     # Interpolate.
     xs_interp = interp(energy, xs_energy, xs_log)
