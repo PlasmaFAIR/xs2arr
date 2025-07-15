180f4ec9752c0e7abc9cab4eb3e065e05d347117
Ava Dean <ava.dean@york.ac.uk>
2025-07-15 16:46:01 +0100

    Changed numpy import statements to import whole numpy rather than individual functions. Added a logarithmic version of the Arrhenius equation

--- src/xs2arr/model.py
+++ src/xs2arr/model.py
@@ -1,6 +1,6 @@
+import numpy as np
 from lmfit import Model as FittingModel
 from lmfit import Parameters
-from numpy import array, asarray, linspace, ndarray, sqrt
 
 from xs2arr.cross_section import interpolate_xs
 from xs2arr.eedf import Druyvesteyn, Maxwellian
@@ -14,7 +14,7 @@ class Model:
         self,
         lxcat_file: str | None = None,
         eedf_type: str = "maxwellian",
-        eedf_grid: ndarray | None = None,
+        eedf_grid: np.ndarray | None = None,
     ):
         if lxcat_file is None:
             raise ValueError("No lxcat file provided")
@@ -29,16 +29,16 @@ class Model:
             raise ValueError("eedf_type must be 'maxwellian' or 'druyvesteyn'")
 
         if eedf_grid is None:
-            eedf_grid = linspace(start=0.0, stop=100.0, num=10000, dtype=float)
+            eedf_grid = np.linspace(start=0.0, stop=100.0, num=10000, dtype=float)
         if isinstance(eedf_grid, list | tuple):
-            eedf_grid = array(eedf_grid, dtype=float)
-        if not isinstance(eedf_grid, ndarray):
+            eedf_grid = np.array(eedf_grid, dtype=float)
+        if not isinstance(eedf_grid, np.ndarray):
             raise TypeError("eedf_grid must be of type ndarray")
         if eedf_grid.ndim != 1:
             raise ValueError("eedf_grid must be 1-dimensional")
         if len(eedf_grid) < 2:
             raise ValueError("len(eedf_grid) must be >= 2")
-        if any(eedf_grid < 0.0):
+        if np.any(eedf_grid < 0.0):
             raise ValueError("All values in eedf_grid must be >= 0.0")
 
         self.cross_section_set = parse_lxcat_data(lxcat_file)
@@ -46,7 +46,7 @@ class Model:
         self.eedf_grid = eedf_grid
 
     def arrhenius(
-        self, T_grid: ndarray | None = None, mean_E_grid: ndarray | None = None
+        self, T_grid: np.ndarray | None = None, mean_E_grid: np.ndarray | None = None
     ):
         # Only allow a user to call arrhenius from an instantiated object of the class.
         if isinstance(self, type):
@@ -57,18 +57,18 @@ class Model:
         if T_grid is not None and mean_E_grid is not None:
             raise ValueError("Only one of T_grid or mean_E_grid must be provided")
         if T_grid is None and mean_E_grid is None:
-            T_grid = linspace(start=0.001, stop=6.0, num=1000, dtype=float)
+            T_grid = np.linspace(start=0.001, stop=6.0, num=1000, dtype=float)
         if mean_E_grid is not None:
             T_grid = 2.0 * mean_E_grid / 3.0
         if isinstance(T_grid, list | tuple):
-            T_grid = array(T_grid, dtype=float)
-        if not isinstance(T_grid, ndarray):
+            T_grid = np.array(T_grid, dtype=float)
+        if not isinstance(T_grid, np.ndarray):
             raise TypeError("T_grid must be of type ndarray")
         if T_grid.ndim != 1:
             raise ValueError("T_grid must be 1-dimensional")
         if len(T_grid) < 2:
             raise ValueError("len(T_grid) must be >= 2")
-        if any(T_grid < 0.0):
+        if np.any(T_grid < 0.0):
             raise ValueError("All values in T_grid must be >= 0.0")
 
         # For the Arrhenius equation fittings.
@@ -80,8 +80,8 @@ class Model:
         results = []
 
         for cross_section_info in self.cross_section_set.cross_sections:
-            xs_energy = asarray(cross_section_info.data["energy"], dtype=float)
-            xs = asarray(cross_section_info.data["cross section"], dtype=float)
+            xs_energy = np.asarray(cross_section_info.data["energy"], dtype=float)
+            xs = np.asarray(cross_section_info.data["cross section"], dtype=float)
 
             # Interpolate the cross section onto the EEDF grid.
             xs_interp = interpolate_xs(self.eedf_grid, xs_energy, xs)
@@ -94,10 +94,10 @@ class Model:
                 for T in T_grid
             ]
 
-            rates = asarray(rates, dtype=float)
+            rates = np.asarray(rates, dtype=float)
 
             # Convert rates to appropriate units.
-            rates *= sqrt(2.0 * q / m_e)
+            rates *= np.sqrt(2.0 * q / m_e)
 
             # And compute the a, b and c Arrhenius coefficients.
             regressor = FittingModel(arrhenius, independent_vars=["T"])
--- src/xs2arr/rate.py
+++ src/xs2arr/rate.py
@@ -1,5 +1,7 @@
-from numpy import ndarray, sqrt, trapezoid
+import numpy as np
 
 
-def compute_rate(energies: ndarray, cross_section: ndarray, eedf: ndarray) -> float:
-    return trapezoid(sqrt(energies) * cross_section * eedf, x=energies)
+def compute_rate(
+    energies: np.ndarray, cross_section: np.ndarray, eedf: np.ndarray
+) -> float:
+    return np.trapezoid(np.sqrt(energies) * cross_section * eedf, x=energies)
