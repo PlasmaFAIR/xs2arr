351a8655392d37b68aedb97867990f8093495519
Ava Dean <ava.dean@york.ac.uk>
2025-07-22 17:27:18 +0100

    Changed Kelvin to eV in temperature units

--- src/xs2arr/model.py
+++ src/xs2arr/model.py
@@ -42,7 +42,7 @@ class Model:
         ----------
         T_grid : optional
             Temperature grid for rate calculations. If None and mean_E_grid is None,
-            a default grid of 1000 points between 0.001 and 6 K will be used.
+            a default grid of 1000 points between 0.001 and 6 eV will be used.
         mean_E_grid : optional
             Mean energy grid to define the temperature grid. If provided, T_grid will be
             calculated as 2/3 of mean_E_grid.
@@ -220,7 +220,7 @@ def _validate_and_prepare_T_grid(
     ----------
     T_grid
         Temperature grid for rate calculations. If None and mean_E_grid is None,
-        a default grid of 1000 points between 0.001 and 6 K will be used.
+        a default grid of 1000 points between 0.001 and 6 eV will be used.
     mean_E_grid
         Mean energy grid to define the temperature grid. If provided, T_grid will be
         calculated as 2/3 of mean_E_grid.
